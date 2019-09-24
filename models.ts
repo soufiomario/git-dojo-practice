eb5e9b177be88b7a4a16d88bd5a5bc98f4cf3ad3
Omar Soufi <Omar.Soufi@ensimag.grenoble-inp.fr>
2019-09-24 15:17:56 +0200

    ajout 2

--- models.ts
+++ models.ts
@@ -3,3 +3,4 @@
 // Dans la branche 'fonctionnalité / mise en forme': reformater le type sur plusieurs lignes pour une meilleure lisibilité
 type  Enseignant  = {nom :  chaîne ; année :  ' 1A '  |  ' 2A '  |  " 3A " ; email :  chaîne }
 dd
+ff
