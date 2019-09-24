2cee66b83248b6a4687b503ac0e4e8280265862f
Omar Soufi <Omar.Soufi@ensimag.grenoble-inp.fr>
2019-09-24 15:17:40 +0200

    ajout

--- models.ts
+++ models.ts
@@ -1,4 +1,5 @@
 
-// In branch 'fix/typo': rename the type to Student
-// In branch 'feature/formatting': reformat the type on multiple lines for better readibility
-type Teacher = { name: string; year: '1A' | '2A' | '3A'; email: string }
+// Dans la branche 'fix / typo': renomme le type en Etudiant
+// Dans la branche 'fonctionnalité / mise en forme': reformater le type sur plusieurs lignes pour une meilleure lisibilité
+type  Enseignant  = {nom :  chaîne ; année :  ' 1A '  |  ' 2A '  |  " 3A " ; email :  chaîne }
+dd
